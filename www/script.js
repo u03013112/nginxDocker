9049b1237055cc285053db29b0f3649c9344f765
u03013112 <u03013112@hotmail.com>
2022-01-02 22:06:44 +0800

    test 1

---
+++ www/script.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
