8620522deecde842467e909daa3692d2afe65e9d
u03013112 <u03013112@hotmail.com>
2022-02-16 18:32:01 +0800

    test 1

--- www/script.js
+++ www/script.js
@@ -1,17 +1,54 @@
-var app = new Vue({
-    el: '#app',
+var goldMine = new Vue({
+    el: '#goldMine',
     data: {
-        message: 'Hello Vue!'
+        buildingLevel:10,
+        mergeLevel:12,
+        buildMax:10,
+        statusStrs:[
+            {text:'Level 58 count:10'},
+            {text:'Level 58 count:10'},
+            {text:'Level 58 count:10'},
+        ]
     }
 })
-var app5 = new Vue({
-    el: '#app-5',
+
+var barrack = new Vue({
+    el: '#barrack',
     data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
+        buildingLevel:11,
+        mergeLevel:15,
+        buildMax:16,
+        statusStrs:[
+            {text:'Level 58 count:11'},
+            {text:'Level 58 count:12'},
+            {text:'Level 58 count:13'},
+        ]
+    }
+})
+
+var shipyard = new Vue({
+    el: '#shipyard',
+    data: {
+        buildingLevel:11,
+        mergeLevel:15,
+        buildMax:16,
+        statusStrs:[
+            {text:'Level 58 count:11'},
+            {text:'Level 58 count:12'},
+            {text:'Level 58 count:13'},
+        ]
+    }
+})
+
+
+var hotkey = new Vue({
+    el: '#hotkey',
+    data: {
+        hotkeys:[
+            {text:'Escape : Skip the battle'},
+            {text:'Escape : Quit from some menu'},
+            {text:'q : Quick deploy in battle'},
+            {text:'Enter: Start battle'}
+        ]
     }
 })
