6ea884be681ee222cb54a2cc1870179055d61f07
u03013112 <u03013112@hotmail.com>
2022-02-25 18:06:06 +0800

    初级改版完成

--- www/script.js
+++ www/script.js
@@ -1,54 +1,42 @@
-var goldMine = new Vue({
-    el: '#goldMine',
+var app = new Vue({
+    el: '#app',
     data: {
-        buildingLevel:10,
-        mergeLevel:12,
-        buildMax:10,
-        statusStrs:[
-            {text:'Level 58 count:10'},
-            {text:'Level 58 count:10'},
-            {text:'Level 58 count:10'},
-        ]
-    }
-})
-
-var barrack = new Vue({
-    el: '#barrack',
-    data: {
-        buildingLevel:11,
-        mergeLevel:15,
-        buildMax:16,
-        statusStrs:[
-            {text:'Level 58 count:11'},
-            {text:'Level 58 count:12'},
-            {text:'Level 58 count:13'},
-        ]
-    }
-})
-
-var shipyard = new Vue({
-    el: '#shipyard',
-    data: {
-        buildingLevel:11,
-        mergeLevel:15,
-        buildMax:16,
-        statusStrs:[
-            {text:'Level 58 count:11'},
-            {text:'Level 58 count:12'},
-            {text:'Level 58 count:13'},
-        ]
-    }
-})
-
-
-var hotkey = new Vue({
-    el: '#hotkey',
-    data: {
-        hotkeys:[
-            {text:'Escape : Skip the battle'},
-            {text:'Escape : Quit from some menu'},
-            {text:'q : Quick deploy in battle'},
-            {text:'Enter: Start battle'}
-        ]
+        notReady: true,
+        hotkeys: [
+            { text: 'Escape : Skip the battle' },
+            { text: 'Escape : Quit from some menu' },
+            { text: 'q : Quick deploy in battle' },
+            { text: 'Enter: Start battle' }
+        ],
+        goldMine: {
+            buildingLevel: 10,
+            mergeLevel: 12,
+            buildMax: 10,
+            statusStrs: [
+                { text: 'Level 58 count:10' },
+                { text: 'Level 58 count:10' },
+                { text: 'Level 58 count:10' },
+            ]
+        },
+        barrack: {
+            buildingLevel: 11,
+            mergeLevel: 15,
+            buildMax: 16,
+            statusStrs: [
+                { text: 'Level 58 count:11' },
+                { text: 'Level 58 count:12' },
+                { text: 'Level 58 count:13' },
+            ]
+        },
+        shipyard: {
+            buildingLevel: 11,
+            mergeLevel: 15,
+            buildMax: 16,
+            statusStrs: [
+                { text: 'Level 58 count:11' },
+                { text: 'Level 58 count:12' },
+                { text: 'Level 58 count:13' },
+            ]
+        }
     }
 })
