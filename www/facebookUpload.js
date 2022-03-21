b49af6a5f4cfb0f6b92e2365a45bf5f7afcb12ca
u03013112 <u03013112@hotmail.com>
2022-03-21 19:37:55 +0800

    基本完成

--- www/facebookUpload.js
+++ www/facebookUpload.js
@@ -14,6 +14,7 @@ var facebookUpload = new Vue({
         reflushAdAccountBtnClick:function(){
             this.$http.get('http://192.168.40.62:8080/facebookAccounts').then(response => response.json()).then( json=>{
                 this.adAccounts = json;
+                this.outText = '刷新完成'
             },function(){
                 this.outText = '请求失败处理';
             });
@@ -35,11 +36,29 @@ var facebookUpload = new Vue({
                 var sizes = this.searchSize.split(" ");
                 data.size=sizes
             }
-            console.log(data)
             
             this.outText = "正在搜索 请耐心等待，大约30秒"
             this.$http.post('http://192.168.40.62:8080/searchFromWarehouse',data).then(response => response.json()).then( json=>{
                 this.filePaths = json
+                this.outText = '搜索完成'
+            },function(res){
+                this.outText = '请求失败处理';
+            });
+        },
+        uploadBtnClick:function(){
+            if (this.selectedAdAccounts.length <= 0){
+                this.outText = '请选择需要上传账号'
+                return
+            }
+            if (this.selectedFilePaths.length <= 0){
+                this.outText = '请选择需要上传文件'
+                return
+            }
+            this.outText = "正在上传 请耐心等待，文件和账号越多，时间越长，慢慢等待"
+            var data = {"accountIds":this.selectedAdAccounts,"filePathList":this.selectedFilePaths}
+            this.$http.post('http://192.168.40.62:8080/facebookUpload',data).then(response => response.json()).then( json=>{
+                this.filePaths = json
+                this.outText = '上传完成，有时候FB反应略慢，等几秒刷新FB页面就有了'
             },function(res){
                 this.outText = '请求失败处理';
             });
